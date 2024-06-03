04d961ec8c0bcd102d6eaf454380a0054101cd7f
Ajis Dzalparo <ajisdzalparo22@gmail.com>
2024-06-03 20:10:52 +0700

    Menambahkan animasi dan memperbaiki playing musik

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 document.getElementById("surpriseButton").addEventListener("click", function () {
   const surpriseMessage = document.getElementById("surpriseMessage");
-  if (surpriseMessage.classList.contains("hidden")) {
-    surpriseMessage.classList.remove("hidden");
+  const backgroundMusic = document.getElementById("backgroundMusic");
+
+  surpriseMessage.classList.toggle("hidden");
+
+  // Play the music if it is not already playing
+  if (backgroundMusic.paused) {
+    backgroundMusic.play();
   } else {
-    surpriseMessage.classList.add("hidden");
+    backgroundMusic.pause();
   }
 });
+
 window.onload = function () {
   const backgroundMusic = document.getElementById("backgroundMusic");
-  backgroundMusic.play();
+  // Check if the audio context is allowed to autoplay
+  backgroundMusic.play().catch(function () {
+    // Autoplay was prevented, show a message to the user
+    console.log("Autoplay was prevented, click to play music");
+  });
 };
